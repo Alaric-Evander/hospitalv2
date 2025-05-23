95f870ef70696f0438b74045d2ea3b30af4ac39b
BOUSMARA Yassine <y.bousmara@edu.umi.ac.ma>
2025-05-23 16:12:46 +0100

    Push 6

--- src/main/java/com/example/hospitalv2/demo/web/PatientController.java
+++ src/main/java/com/example/hospitalv2/demo/web/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -18,9 +19,10 @@ public class PatientController {
 
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model,int page, int size){
+    public String index(Model model, @RequestParam(name="page",defaultValue = "0") int page,@RequestParam(name="size",defaultValue = "4") int size){
         Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
         model.addAttribute("ListPatients", pagePatients.getContent());
+        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
         return "patients";
     }
 }
