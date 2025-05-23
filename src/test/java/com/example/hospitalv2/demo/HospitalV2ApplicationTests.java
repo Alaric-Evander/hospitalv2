d44db77e2ec8dca515095083947243f11fb2b3f2
BOUSMARA Yassine <y.bousmara@edu.umi.ac.ma>
2025-05-23 09:12:29 +0100

    Initial commit

---
+++ src/test/java/com/example/hospitalv2/demo/HospitalV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalv2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
