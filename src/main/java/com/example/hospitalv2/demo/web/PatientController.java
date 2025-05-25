123a0901c0890599295976c9932da0de84ece423
BOUSMARA Yassine <y.bousmara@edu.umi.ac.ma>
2025-05-25 17:41:04 +0100

    Push 4 Partie2

--- src/main/java/com/example/hospitalv2/demo/web/PatientController.java
+++ src/main/java/com/example/hospitalv2/demo/web/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -40,6 +41,12 @@ public class PatientController {
     @GetMapping("/formPatients")
     public String formPatient(Model model){
         model.addAttribute("patient", new Patient());
+        return "formPatients";
+    }
+
+    @PostMapping(path ="/save")
+    public String save(Model model, Patient patient){
+        patientRepository.save(patient);
         return "formPatients";
     }
 }
